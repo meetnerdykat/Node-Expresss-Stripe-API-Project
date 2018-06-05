464d0f7e17c40cd8b70db51389771f052ba07940
Kathy Cuevas <meetnerdykat@gmail.com>
2018-06-05 19:15:31 -0400

    changed back to reg js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,2 +1,4 @@
-export const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
-export const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+module.exports = {
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+} 
